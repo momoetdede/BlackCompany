752e4aeae581ca2f5f38f955095973d317fdc695
Romain <ro9178@hotmail.fr>
2017-10-08 19:55:43 +0200

    MVC change
    Signed-off-by: Romain <ro9178@hotmail.fr>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,50 @@
 'use strict';
 
+
+/**
+ * @ngdoc function
+ * @name blackCompanyApp.controller:HeaderCtrl
+ * @description
+ * # HeaderCtrl
+ * Controller of the header
+ */
+
+angular.module('blackCompanyApp')
+.controller('HeaderCtrl', function ($scope) {
+  $scope.header_name = "Header"
+});
+
+
+/**
+ * @ngdoc function
+ * @name blackCompanyApp.controller:FooterCtrl
+ * @description
+ * # FooterCtrl
+ * Controller of the footer
+ */
+
+angular.module('blackCompanyApp')
+.controller('FooterCtrl', function ($scope) {
+  $scope.footer_name = "Footer"
+});
+ 
+
+
+/**
+ * @ngdoc function
+ * @name blackCompanyApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the footer
+ */
+
+ angular.module('blackCompanyApp')
+ .controller('MenuCtrl', function ($scope) {
+   $scope.menu_name = "Menu"
+ });
+ 
+
+
 /**
  * @ngdoc function
  * @name blackCompanyApp.controller:MainCtrl
@@ -7,7 +52,24 @@
  * # MainCtrl
  * Controller of the blackCompanyApp
  */
+
+//Main Controller
 angular.module('blackCompanyApp')
 .controller('MainCtrl', function ($scope) {
   $scope.name = "Toubib"
 });
+
+
+/**
+ * @ngdoc function
+ * @name blackCompanyApp.controller:ContentCtrl
+ * @description
+ * # ContentCtrl
+ * Controller of the Content
+ */
+
+angular.module('blackCompanyApp')
+.controller('ContentCtrl', function ($scope) {
+  $scope.content_name = "Content"
+});
+
