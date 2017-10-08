9b36ac0431c334a7bf87b9a4872921ac9f8523c6
Romain <ro9178@hotmail.fr>
2017-10-08 13:54:28 +0200

    Creation du projet Angular

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name blackCompanyApp
+ * @description
+ * # blackCompanyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('blackCompanyApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blackCompanyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blackCompanyApp
+ */
+angular.module('blackCompanyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
